7d0ec2e38041d76e067b6d4fd1505c81a505a061
lz2712 <leizeraponte2020@gmail.com>
2025-06-17 15:03:40 -0400

    Subiendo el Modal de Permisos, (Haciendo validaciones de los módulos, por instalar)

---
+++ config/modulo.php
@@ -0,0 +1,102 @@
+<?php
+
+const modulos = [
+    [
+        'id' => 1,
+        'modulo' => 'usuario'
+    ],
+    [
+        'id' => 2,
+        'modulo' => 'rol'
+    ],
+    [
+        'id' => 3,
+        'modulo' => 'bitacora'
+    ],
+    [
+        'id' => 4,
+        'modulo' => 'mantenimiento'
+    ],
+    [
+        'id' => 5,
+        'modulo' => 'empleado'
+    ],
+    [
+        'id' => 6,
+        'modulo' => 'tecnico'
+    ],
+    [
+        'id' => 7,
+        'modulo' => 'solicitud'
+    ],
+    [
+        'id' => 8,
+        'modulo' => 'hoja_servicio'
+    ],
+    [
+        'id' => 9,
+        'modulo' => 'ente'
+    ],
+    [
+        'id' => 10,
+        'modulo' => 'dependencia'
+    ],
+    [
+        'id' => 11,
+        'modulo' => 'unidad'
+    ],
+    [
+        'id' => 12,
+        'modulo' => 'cargo'
+    ],
+    [
+        'id' => 13,
+        'modulo' => 'tipo_servicio'
+    ],
+    [
+        'id' => 14,
+        'modulo' => 'bien'
+    ],
+    [
+        'id' => 15,
+        'modulo' => 'tipo_bien'
+    ],
+    [
+        'id' => 16,
+        'modulo' => 'marca'
+    ],
+    [
+        'id' => 17,
+        'modulo' => 'equipo'
+    ],
+    [
+        'id' => 18,
+        'modulo' => 'switch'
+    ],
+    [
+        'id' => 19,
+        'modulo' => 'patch_panel'
+    ],
+    [
+        'id' => 20,
+        'modulo' => 'interconexion'
+    ],
+    [
+        'id' => 21,
+        'modulo' => 'punto_conexion'
+    ],
+    [
+        'id' => 22,
+        'modulo' => 'piso'
+    ],
+    [
+        'id' => 23,
+        'modulo' => 'oficina'
+    ],
+    [
+        'id' => 24,
+        'modulo' => 'material'
+    ]
+];
+
+?>
