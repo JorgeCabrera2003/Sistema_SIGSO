691d8c71f38990c78650d94be82c1212897fae74
lz2712 <leizeraponte2020@gmail.com>
2025-07-01 15:39:30 -0400

    Instalación de Permisos en el módulo de Pisos con Validaciones del controlador

--- view/piso.php
+++ view/piso.php
@@ -1,8 +1,8 @@
-<?php require_once("Componentes/head.php");?>
+<?php require_once("Componentes/head.php"); ?>
 
 <body>
-<?php require_once("Componentes/menu.php"); 
-require_once("Componentes/modal_piso.php");?>
+  <?php require_once("Componentes/menu.php");
+  require_once("Componentes/modal_piso.php"); ?>
 
 
   <main id="main" class="main">
@@ -11,7 +11,8 @@ require_once("Componentes/modal_piso.php");?>
       <nav>
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="">Home</a></li>
-          <li class="breadcrumb-item active"><a href="">Gestionar Pisos<</a></li>
+          <li class="breadcrumb-item active"><a href="">Gestionar Pisos </a>
+          </li>
         </ol>
       </nav>
     </div><!-- End Page Title -->
@@ -22,21 +23,24 @@ require_once("Componentes/modal_piso.php");?>
 
           <div class="card">
             <div class="card-body">
-              <h5 class="card-title">Gestionar Pisos</h5>     
-              <button type="button" class="btn btn-primary mx-auto my-4" id="btn-registrar">
-      Registrar Piso
-    </button>
+              <h5 class="card-title">Gestionar Pisos</h5>
+              <?php if (isset($permisos['piso']['registrar']['estado']) && $permisos['piso']['registrar']['estado'] == '1') { ?>
+                <button type="button" class="btn btn-primary mx-auto my-4" id="btn-registrar">
+                  Registrar Piso
+                </button>
+              <?php } ?>
               <div class="table-responsive">
-                  <table class="table" id="tabla1" style="width: 1024px;">
-                      <thead>
-                          <tr>
-                              <?php foreach ($cabecera as $campo) echo "<th scope='col'>$campo</th>"; ?>
-                          </tr>
-                      </thead>
-                      <tbody>
+                <table class="table" id="tabla1" style="width: 1024px;">
+                  <thead>
+                    <tr>
+                      <?php foreach ($cabecera as $campo)
+                        echo "<th scope='col'>$campo</th>"; ?>
+                    </tr>
+                  </thead>
+                  <tbody>
 
-                      </tbody>
-                  </table>
+                  </tbody>
+                </table>
               </div>
             </div>
           </div>
@@ -51,7 +55,8 @@ require_once("Componentes/modal_piso.php");?>
   <!-- ======= Footer ======= -->
   <?php require_once "Componentes/footer.php"; ?>
 
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
   <script defer src="assets/js/piso.js"></script>
 
 </body>
