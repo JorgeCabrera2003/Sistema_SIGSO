caedb680f4c0de773d8d5c6b958d302984f318a7
lz2712 <leizeraponte2020gmail.com>
2025-10-20 18:51:31 -0400

    Actualizando Test

--- test/PisoTest.php
+++ test/PisoTest.php
@@ -7,7 +7,6 @@ require_once "model/piso.php";
 
 final class PisoTest extends TestCase
 {
-
     private Piso $Tpiso;
 
     public function setUp(): void
@@ -25,14 +24,15 @@ final class PisoTest extends TestCase
 
         $this->assertIsArray($resultado);
 
-        if ($resultado['estado']  == 1) {
+        if (isset($resultado['estado']) && $resultado['estado'] == 1) {
             $this->assertEquals('registrar', $resultado['resultado']);
             $this->assertEquals('1', $resultado['estado']);
         } else if ($resultado['estado'] == -1) {
             $this->assertEquals('error', $resultado['resultado']);
             $this->assertEquals('Registro duplicado', $resultado['mensaje']);
+        } else {
+            $this->fail('Fallo en Registrar Piso');
         }
-
     }
 
     public function testConsultarPiso()
@@ -43,6 +43,40 @@ final class PisoTest extends TestCase
         $this->assertIsArray($resultado);
         $this->assertEquals('consultar', $resultado['resultado']);
         $this->assertIsArray($resultado['datos']);
+    }
+
+    public function testModificarPiso()
+    {
+
+        $this->Tpiso->set_id("SOTAN0012025101912552354");
+        $this->Tpiso->set_tipo('Sótano');
+        $this->Tpiso->set_nro_piso(3);
+        $resultado = $this->Tpiso->Transaccion(['peticion' => 'actualizar']);
+
+        $this->assertIsArray($resultado);
+
+        if (isset($resultado['estado']) && $resultado['estado'] == 1) {
+            $this->assertEquals('modificar', $resultado['resultado']);
+            $this->assertEquals('1', $resultado['estado']);
+        } else if ($resultado['estado'] == -1) {
+            $this->fail('Fallo en Modificar Piso');
+        }
+
+    }
+
+    public function testEliminarPiso()
+    {
+        $this->Tpiso->set_id("SOTAN0012025101912552354");
+        $resultado = $this->Tpiso->Transaccion(['peticion' => 'eliminar']);
+
+        $this->assertIsArray($resultado);
+
+        if (isset($resultado['estado']) && $resultado['estado'] == 1) {
+            $this->assertEquals('eliminar', $resultado['resultado']);
+            $this->assertEquals('1', $resultado['estado']);
+        } else if ($resultado['estado'] == -1) {
+            $this->fail('Fallo en Eliminar Piso');
+        }
     }
 }
 
