6470c467dc4fbaa364b43511ef9bf0026e3b5ab8
lz2712 <leizeraponte2020gmail.com>
2025-10-20 20:18:26 -0400

    Subiendo test de prueba de Ente

--- test/EnteTest.php
+++ test/EnteTest.php
@@ -1,26 +1,28 @@
 <?php
 declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
-require_once "model/piso.php";
+require_once "model/ente.php";
 
 #[ExpectException(ValueError::class)]
 
-final class PisoTest extends TestCase
+final class EnteTest extends TestCase
 {
-    private Piso $Tpiso;
+    private Ente $Tente;
 
     public function setUp(): void
     {
-        $this->Tpiso = new Piso();
+        $this->Tente = new Ente();
     }
 
-    public function testRegistrarPiso()
+    public function testRegistrarEnte()
     {
 
-        $this->Tpiso->set_id("SOTAN0012025101912552354");
-        $this->Tpiso->set_tipo('Sótano');
-        $this->Tpiso->set_nro_piso(2);
-        $resultado = $this->Tpiso->Transaccion(['peticion' => 'registrar']);
+        $this->Tente->set_id("PARQU0122025201014043321");
+        $this->Tente->set_nombre('Parque del Este');
+        $this->Tente->set_telefono('0424-9883212');
+        $this->Tente->set_direccion('Avenida Los Horcones');
+        $this->Tente->set_responsable('Antonia de Gil');
+        $resultado = $this->Tente->Transaccion(['peticion' => 'registrar']);
 
         $this->assertIsArray($resultado);
 
@@ -30,33 +32,33 @@ final class PisoTest extends TestCase
         } else if ($resultado['estado'] == -1) {
             if ($resultado['mensaje'] == "Registro duplicado") {
                 $this->assertTrue(true, "No permitir registros duplicados");
-            } else if ($resultado['mensaje'] == "Ya hay un piso con este mismo número") {
-                $this->assertTrue(true, "No permitir pisos repetidos");
             } else {
                 $this->assertTrue(false, $resultado['mensaje']);
             }
         } else {
-            $this->fail('Fallo en Registrar Piso');
+            $this->fail('Fallo en Registrar Ente');
         }
     }
 
-    public function testConsultarPiso()
+    public function testConsultarEnte()
     {
 
-        $resultado = $this->Tpiso->Transaccion(['peticion' => 'consultar']);
+        $resultado = $this->Tente->Transaccion(['peticion' => 'consultar']);
 
         $this->assertIsArray($resultado);
         $this->assertEquals('consultar', $resultado['resultado']);
         $this->assertIsArray($resultado['datos']);
     }
 
-    public function testModificarPiso()
+    public function testModificarEnte()
     {
 
-        $this->Tpiso->set_id("SOTAN0012025101912552354");
-        $this->Tpiso->set_tipo('Sótano');
-        $this->Tpiso->set_nro_piso(2);
-        $resultado = $this->Tpiso->Transaccion(['peticion' => 'actualizar']);
+        $this->Tente->set_id("PARQU0122025201014043321");
+        $this->Tente->set_nombre('Parque del Oeste');
+        $this->Tente->set_telefono('0424-9883212');
+        $this->Tente->set_direccion('Avenida Los Horcones');
+        $this->Tente->set_responsable('Antonia de Gil');
+        $resultado = $this->Tente->Transaccion(['peticion' => 'actualizar']);
 
         $this->assertIsArray($resultado);
 
@@ -67,15 +69,15 @@ final class PisoTest extends TestCase
             if ($resultado['mensaje'] == "Ya hay un piso con este mismo número") {
                 $this->assertTrue(true, "No permitir pisos repetidos");
             } else {
-                $this->fail('Fallo en Modificar Piso');
+                $this->fail('Fallo en Modificar Ente');
             }
         }
     }
 
-    public function testEliminarPiso()
+    public function testEliminarEnte()
     {
-        $this->Tpiso->set_id("SOTAN0012025101912552354");
-        $resultado = $this->Tpiso->Transaccion(['peticion' => 'eliminar']);
+        $this->Tente->set_id("PARQU0122025201014043321");
+        $resultado = $this->Tente->Transaccion(['peticion' => 'eliminar']);
 
         $this->assertIsArray($resultado);
 
@@ -83,7 +85,7 @@ final class PisoTest extends TestCase
             $this->assertEquals('eliminar', $resultado['resultado']);
             $this->assertEquals('1', $resultado['estado']);
         } else if ($resultado['estado'] == -1) {
-            $this->fail('Fallo en Eliminar Piso');
+            $this->fail('Fallo en Eliminar Ente');
         }
     }
 }
